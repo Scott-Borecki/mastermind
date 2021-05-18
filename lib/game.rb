c76f61c1d26af3203a9c652e80d38a0aa15d00b3
Scott-Borecki <scottborecki@gmail.com>
2021-05-17 20:05:47 -0600

    Create functioning game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 class Game
 
   def initialize
+    @guess_count = 0
   end
 
   def start_message
@@ -13,23 +14,24 @@ class Game
     @input = gets.chomp.downcase
     if @input == 'p' || @input == "play"
       generate_sequence
+      @time1 = Time.now
       game_flow
     elsif @input == 'i' || @input == 'instructions'
       puts "Instructions will go here, now that you've got it:"
       puts ""
       puts ""
-        start_message
+      start_message
       self.start_input
     elsif @input == 'q' || @input == 'quit'
     else
       p "That seemed tough, try again"
-        self.start_input
+      self.start_input
     end
   end
 
   def generate_sequence
-    sequence = Sequence.new
-    sequence.generate
+    @sequence = Sequence.new
+    @sequence.generate
   end
 
   def game_flow
@@ -39,27 +41,39 @@ class Game
     @guess = gets.chomp
     if @guess == 'q' || @guess == 'quit'
     elsif @guess == 'c' || @guess == 'cheat'
-      puts sequence.secret_code.join.upcase
+      puts @sequence.secret_code.join.upcase
       self.game_flow
     elsif @guess.length > 4
       puts "That's too long!"
     elsif @guess.length < 4
       puts "That's too short!"
     elsif @guess.length == 4
+      @guess_count += 1
       evaluate_guess
     end
   end
 
   def evaluate_guess
-    puts "'#{@guess.upcase}' has #{@num_correct_total} of the correct elements with #{@num_correct_position} in the correct positions"
-    puts "You've taken #{guess_count} guess"
+    if num_correct_position == 4
+      @time2 = Time.now
+      @elapsed_seconds = ((@time2 - @time1) % 60).round
+      @elapsed_minutes = ((@time2 - @time1) / 60).floor
+      puts "Congratulations! You guessed the sequence '#{@guess.upcase}' in #{@guess_count} guesses over #{@elapsed_minutes} minutes, #{@elapsed_seconds} seconds."
+      puts ""
+      puts "Do you want to (p)lay again or (q)uit"
+      start_input
+    else # @num_correct_position < 4
+      puts "'#{@guess.upcase}' has #{num_correct_total} of the correct elements with #{num_correct_position} in the correct positions"
+      puts "You've taken #{@guess_count} guess" # update for single guess
+      game_flow
+    end
   end
 
   def num_correct_total
-    # @guess_array = @guess.split(//)
-    # @sequence = sequence.secret_code
-    @guess_array = ['r', 'g', 'b', 'y']
-    @sequence = ['y','y','y','y']
+    @guess_array = @guess.split(//)
+    @sequence_array = @sequence.secret_code
+    # @guess_array = ['r', 'g', 'b', 'y']
+    # @sequence = ['y','y','y','y']
 
     tally_r_guess = @guess_array.count do |letter|
       letter.include?("r")
@@ -74,16 +88,16 @@ class Game
       letter.include?("y")
     end
 
-    tally_r_code = @sequence.count do |letter|
+    tally_r_code = @sequence_array.count do |letter|
       letter.include?("r")
     end
-    tally_g_code = @sequence.count do |letter|
+    tally_g_code = @sequence_array.count do |letter|
       letter.include?("g")
     end
-    tally_b_code = @sequence.count do |letter|
+    tally_b_code = @sequence_array.count do |letter|
       letter.include?("b")
     end
-    tally_y_code = @sequence.count do |letter|
+    tally_y_code = @sequence_array.count do |letter|
       letter.include?("y")
     end
 
@@ -102,30 +116,28 @@ class Game
 
     @num_correct_total = tally_r.min + tally_g.min + tally_b.min + tally_y.min
 
-    require "pry"; binding.pry
-
   end
 
   def num_correct_position
     @guess_array = @guess.split(//)
-    @sequence = sequence.secret_code
+    @sequence_array = @sequence.secret_code
     @num_correct_position = 0
-    if @guess_array[0] == @sequence[0]
+    if @guess_array[0] == @sequence_array[0]
       @num_correct_position += 1
     end
-    if @guess_array[1] == @sequence[1]
+    if @guess_array[1] == @sequence_array[1]
       @num_correct_position += 1
     end
-    if @guess_array[2] == @sequence[2]
+    if @guess_array[2] == @sequence_array[2]
       @num_correct_position += 1
     end
-    if @guess_array[3] == @sequence[3]
+    if @guess_array[3] == @sequence_array[3]
       @num_correct_position += 1
     end
+    @num_correct_position
   end
 
-  def quit
-  end
-
+  # def quit
+  # end
 
 end
