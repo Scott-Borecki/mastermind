b3a6c0e02dad4266a50fc09fcbc2747c04a08908
Scott-Borecki <scottborecki@gmail.com>
2021-05-18 18:42:15 -0600

    Try to make a test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,9 +10,17 @@ RSpec.describe Game do
       game = Game.new
       expect(game).to be_a(Game)
     end
+  end
 
-    
-
+  describe 'Object Methods' do
+    # it '#num_correct_position' do
+    #   game = Game.new
+    #   sequence = Sequence.new
+    #   @guess = "rrrr"
+    #   sequence.generate
+    #   # @sequence.secret_code = ['r','r','r','r']
+    #   expect(game.num_correct_position).to eq(4)
+    # end
   end
 
 end
