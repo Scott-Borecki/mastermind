8b7b874d532c321349131d9ce82057afe7a24af9
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-05-20 15:32:19 -0400

    Refactor and update sequence tests

--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -4,32 +4,39 @@ require './lib/sequence'
 
 RSpec.describe Sequence do
 
+  before(:each) do
+    @sequence = Sequence.new
+  end
+
   describe 'Object Creation' do
 
     it 'exists' do
-      sequence = Sequence.new
-      expect(sequence).to be_a(Sequence)
+      expect(@sequence).to be_a(Sequence)
     end
 
     it 'has readable attributes' do
-      sequence = Sequence.new
-      expect(sequence.secret_code).to eq([])
+      expect(@sequence.secret_code).to eq([])
     end
-
   end
 
   describe 'Object Methods' do
 
     it '#generate' do
-      sequence = Sequence.new
-
-      sequence.generate
-      expect(sequence.secret_code).to be_a(Array)
-      expect(sequence.secret_code.length).to eq(4)
-      expect(sequence.secret_code.none?(/[rgby]/)).to be false
-      expect(sequence.secret_code.all?(/[rgby]/)).to be true
+      @sequence.generate
+      expect(@sequence.secret_code).to be_a(Array)
+      expect(@sequence.secret_code.length).to eq(4)
+      expect(@sequence.secret_code.none?(/[rgby]/)).to be false
+      expect(@sequence.secret_code.all?(/[rgby]/)).to be true
     end
 
-  end
+    it "has a cheat code" do
+      secret_code = ['r', 'g', 'b', 'y']
+      @sequence.secret_code << secret_code
+      @sequence.secret_code.flatten
+      expected = "RGBY"
 
+      expect(@sequence.cheat_code).to be_a(String)
+      expect(@sequence.cheat_code).to eq(expected)
+    end
+  end
 end
